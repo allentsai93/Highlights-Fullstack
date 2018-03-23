10ea263d7e13f45cf9e53add940853d218fbedb3
Allen <allentsai93@gmail.com>
2018-03-23 03:02:15 +0000

    added styling changes

--- routes/index.js
+++ routes/index.js
@@ -35,7 +35,7 @@ router.post("/register", function(req, res){
         return res.render("register", {error: err.message});
       } 
       passport.authenticate("local")(req, res, function(){
-          req.flash("success", "Welcome to YelpCamp, " + user.username + ".");
+          req.flash("success", "Welcome to Highlights!, " + user.username + ".");
          res.redirect("/highlights"); 
       });
    });
